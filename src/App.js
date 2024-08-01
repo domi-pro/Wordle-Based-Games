cb0b252aa0b43ed6149b12e67b1e4436f4d4d201
domi-pro <dominika.prokocka@gmail.com>
2024-08-01 15:04:33 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { useEffect } from "react";
+
+const answer = "2127";
 
 export default function App() {
   return (
@@ -17,30 +20,94 @@ function Header() {
   );
 }
 
-function BlockInput() {
+function BlockInput({ index, onChangeValue, rightSpot, notRightSpot }) {
   const [value, setValue] = useState("");
+  function handleChange(e) {
+    if (e.target.value.length > 1) return false;
+    setValue(e.target.value);
+    onChangeValue(index, e.target.value);
+  }
   return (
-    <div className="block-input">
-      <input
-        type="number"
-        maxLength="1"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
+    <div
+      className="block-input"
+      style={{
+        backgroundColor: rightSpot[index]
+          ? "#88D66C"
+          : notRightSpot[index]
+          ? "#F6FB7A"
+          : "transparent",
+      }}
+    >
+      <input type="number" value={value} onChange={(e) => handleChange(e)} />
     </div>
   );
 }
 
-function TableRow({ isActive }) {
-  const [numbers, setNumbers] = useState(0);
-  // spróbować jednak jako string a nie int
-
+function TableRow({ isActive, onSetActiveNum }) {
+  const [rowOutput, setRowOutput] = useState("XXXX");
+  const [rightSpot, setRightSpot] = useState([false, false, false, false]);
+  const [notRightSpot, setNotRightSpot] = useState([
+    false,
+    false,
+    false,
+    false,
+  ]);
+  const isFilled = !rowOutput.includes("X") && rowOutput.length === 4;
+  function handleChangeOutput(index, newValue) {
+    if (newValue !== "") {
+      setRowOutput(
+        (output) => output.slice(0, index) + newValue + output.slice(index + 1)
+      );
+    } else {
+      setRowOutput(
+        (output) => output.slice(0, index) + "X" + output.slice(index + 1)
+      );
+    }
+  }
+  useEffect(() => {
+    if (isFilled) {
+      const greenSpots = rowOutput
+        .split("")
+        .map((char, index) => char === answer[index]);
+      console.log(rowOutput);
+      console.log(greenSpots);
+      const yellowSpots = rowOutput
+        .split("")
+        .map((char) => answer.includes(char));
+      console.log(yellowSpots);
+      setRightSpot(greenSpots);
+      setNotRightSpot(yellowSpots);
+      onSetActiveNum((num) => (num < 5 ? num + 1 : num));
+    }
+  }, [isFilled, onSetActiveNum, rowOutput]);
   return (
     <div className={isActive ? "table-row" : "table-row not-active"}>
-      <BlockInput />
-      <BlockInput />
-      <BlockInput />
-      <BlockInput />
+      <BlockInput
+        index={0}
+        onChangeValue={handleChangeOutput}
+        rightSpot={rightSpot}
+        notRightSpot={notRightSpot}
+      />
+      <BlockInput
+        index={1}
+        onChangeValue={handleChangeOutput}
+        rightSpot={rightSpot}
+        notRightSpot={notRightSpot}
+      />
+      <BlockInput
+        index={2}
+        onChangeValue={handleChangeOutput}
+        rightSpot={rightSpot}
+        notRightSpot={notRightSpot}
+      />
+      <BlockInput
+        index={3}
+        onChangeValue={handleChangeOutput}
+        rightSpot={rightSpot}
+        notRightSpot={notRightSpot}
+      />
+      <p>{rowOutput}</p>
+      <p>{isFilled ? "true" : "false"}</p>
     </div>
   );
 }
@@ -50,11 +117,11 @@ function Table() {
 
   return (
     <div className="table">
-      <TableRow isActive={isActiveNum === 1 ? true : false} />
-      <TableRow isActive={isActiveNum === 2 ? true : false} />
-      <TableRow isActive={isActiveNum === 3 ? true : false} />
-      <TableRow isActive={isActiveNum === 4 ? true : false} />
-      <TableRow isActive={isActiveNum === 5 ? true : false} />
+      <TableRow isActive={isActiveNum === 1} onSetActiveNum={setIsActiveNum} />
+      <TableRow isActive={isActiveNum === 2} onSetActiveNum={setIsActiveNum} />
+      <TableRow isActive={isActiveNum === 3} onSetActiveNum={setIsActiveNum} />
+      <TableRow isActive={isActiveNum === 4} onSetActiveNum={setIsActiveNum} />
+      <TableRow isActive={isActiveNum === 5} onSetActiveNum={setIsActiveNum} />
     </div>
   );
 }
